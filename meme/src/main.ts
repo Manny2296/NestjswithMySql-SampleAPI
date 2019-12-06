70db5ddf30a5367562fb93e5107a24bb333dea6d
Manny2296 <manny2296@gmail.com>
2019-12-06 18:14:42 +0100

    Another update

--- meme/src/main.ts
+++ meme/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 //localhost:3000
+const HOST = '0.0.0.0';
+const PORT = Number(process.env.PORT) || 8080;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  app.enableCors();
+  await app.listen(PORT, HOST).then(() => {
+    console.log(`** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`);
+  });
 }
+/*async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+}*/
 bootstrap();
